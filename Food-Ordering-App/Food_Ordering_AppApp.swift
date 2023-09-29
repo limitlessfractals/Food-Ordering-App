5ecfb83e3e0577e42d5050b4009989b5bdc3ad93
Limitless <q@prismatica.cards>
2023-09-28 20:17:16 -0500

    Initial Commit

---
+++ Food-Ordering-App/Food_Ordering_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_Ordering_AppApp.swift
+//  Food-Ordering-App
+//
+//  Created by Wuqiong Fan on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Food_Ordering_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
